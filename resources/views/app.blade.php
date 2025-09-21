1e9352b4cce4d8ff6d4023fad2b773b4f99e9f50
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-09-21 22:09:41 +0300

    systemy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+        <meta name="google-site-verification" content="Bl_5oW_zQJt_6VSXpwYK9ucchhP7gyxpgCZbKMOs4MM" />
         <!-- Scripts -->
         @routes
         @viteReactRefresh
