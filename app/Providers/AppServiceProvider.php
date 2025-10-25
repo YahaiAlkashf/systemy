d811f4cae1f56ea840d87a4b501b334e5dcf390e
yehiaalkashf-sys <yehiaalkashf@gmail.com>
2025-10-26 02:42:49 +0300

    1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
