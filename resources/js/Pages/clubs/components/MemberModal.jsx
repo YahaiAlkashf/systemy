a6639cb74a7ae2ef6a8b51be0be7d8b9b7249a8c
YahaiAlkashf <yeheimohmed@gmail.com>
2025-11-09 15:18:13 +0200

    Update MemberModal.jsx

--- resources/js/Pages/clubs/components/MemberModal.jsx
+++ resources/js/Pages/clubs/components/MemberModal.jsx
@@ -160,7 +160,7 @@ export default function MemberModal({
 
                 <div>
                     <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
-                       {t("المسمى الوظيفي")}
+                       {t(" الكلية")}
                     </label>
                     <input
                         type="text"
